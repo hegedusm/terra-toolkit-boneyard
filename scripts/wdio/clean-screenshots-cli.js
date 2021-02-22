6e44ec2d18e3306b1ea12e3fc4a55f091792d936
mahegedus <mate_hegedus@epam.com>
2021-02-22 11:08:08 +0100

    mark snapshotDirectory as string parameter

--- scripts/wdio/clean-screenshots-cli.js
+++ scripts/wdio/clean-screenshots-cli.js
@@ -8,7 +8,7 @@ const packageJson = require('../../package.json');
 commander
   .version(packageJson.version)
   .option('--removeReference', 'Whether or not to remove the reference screenshots', false)
-  .option('--snapshotDirectory', 'The directory that clean should check for snapshots folders')
+  .option('--snapshotDirectory [string]', 'The directory that clean should check for snapshots folders', undefined)
   .parse(process.argv);
 
 const {
